8956a9f3865749dd6ced8d4841dcdcc511d940f7
thomas <thomaspwthomaspw@gmail.com>
2024-11-15 10:57:59 +0100

    ajout de commentaire et modifications des paramètres de scatter

--- Main.py
+++ Main.py
@@ -1,18 +1,15 @@
 from mimetypes import inited
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
-import random
-import os
-
 from sklearn.metrics import adjusted_rand_score
 
+# Définir les moyennes et la covariance pour les deux classes
 mean = np.array([0,0])
 mean2 = np.array([5,5])
 cov = np.array([[1,0.5],[0.5,1]])
 
+# Générer les données pour chaque classe
 Xclass1 = (np.random.multivariate_normal(mean,cov,128))
 Xclass2 = (np.random.multivariate_normal(mean2,cov,128))
 X = np.vstack((Xclass1,Xclass2))
@@ -24,27 +21,27 @@ true_labels = np.hstack((np.zeros(len(Xclass1)), np.ones(len(Xclass2))))
 
 #plt.scatter(Xclass1[:,0],Xclass1[:,1], label = 'Classe 1', color = 'red')
 
-KMeans = KMeans(n_clusters=100, n_init=10, init='k-means++')
+KMeans = KMeans(n_clusters=2, n_init=10, init='k-means++')
 KMeans.fit(X)
 
 plt.figure(figsize = (12,6))
 
+# Tracer les points avec des symboles et couleurs différents pour chaque classe
 plt.subplot(1,2,1)
 plt.scatter(X[KMeans.labels_ == 0,0],X[KMeans.labels_ == 0,1], c='blue', label="Cluster 1 (k-means", marker='o')
 plt.scatter(X[KMeans.labels_ == 1, 0], X[KMeans.labels_ == 1, 1], c='red', label="Cluster 2 (K-means)", marker='s')
-plt.scatter(KMeans.cluster_centers_[:, 0], KMeans.cluster_centers_[:, 1], c='green', label="Centres des clusters", marker='x')
+plt.scatter(KMeans.cluster_centers_[:, 0], KMeans.cluster_centers_[:, 1], c='green', label="Centres des clusters", marker='o')
 
 plt.subplot(1, 2, 2)
-plt.scatter(X[true_labels == 0, 0], X[true_labels == 0, 1], c='blue', label="Classe 1 (Vraie)", marker='o')
-plt.scatter(X[true_labels == 1, 0], X[true_labels == 1, 1], c='red', label="Classe 2 (Vraie)", marker='s')
+plt.scatter(X[true_labels == 0, 0], X[true_labels == 0, 1], c='red', label="Classe 1 (Vraie)", marker='s')
+plt.scatter(X[true_labels == 1, 0], X[true_labels == 1, 1], c='blue', label="Classe 2 (Vraie)", marker='o')
+
+# # Afficher la légende et les axes
+
 plt.legend()
 plt.xlabel("Mesure 1")
 plt.ylabel("Mesure 2")
 plt.title("Vraie classification")
-
-
-
-#print(kmeans.labels_)
 
 plt.legend()
 plt.xlabel("Mesure 1")
