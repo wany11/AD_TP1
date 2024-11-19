225328f24388374393489acf6cd3e400f44a81ac
thomas <thomaspwthomaspw@gmail.com>
2024-11-19 09:05:09 +0100

    question 1

--- Main.py
+++ Main.py
@@ -1,5 +1,29 @@
+# Importation des bibliothèques nécessaires
 import pandas as pd
+from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.spatial.distance import pdist
+from sklearn.preprocessing import scale
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('temperatures.csv', sep=',', header=0, index_col=0, decimal=',')
-n = len(data)
-data = data.drop
+# Chargement des données
+data_temperature = pd.read_csv("temperatures.csv", sep=";", decimal=".", header=0, index_col=0)
+
+# Suppression des colonnes non pertinentes
+data = data_temperature.drop(columns=['Region', 'Moyenne', 'Amplitude', 'Latitude', 'Longitude'])
+
+# Normalisation des données
+data_scaled = scale(data)
+
+# Calcul de la matrice de dissimilarité
+distance_matrix = pdist(data_scaled, metric='euclidean')
+
+# Construction de l'arbre (méthode ward utilisée ici)
+linkage_matrix = linkage(distance_matrix, method='ward')
+
+# Visualisation de l'arbre
+plt.figure(figsize=(10, 7))
+dendrogram(linkage_matrix, labels=data.index, leaf_rotation=90, leaf_font_size=10)
+plt.title("Dendrogramme - Classification Hiérarchique Ascendante")
+plt.xlabel("Villes")
+plt.ylabel("Distance")
+plt.show()
