df3e33ef1f4c78528ea3dab774850e2100e0ebfc
rsimo <r.simon8603@gmail.com>
2024-11-21 08:58:00 +0100

    Mise au propre

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.cluster import KMeans
 
-img=np.float32(mpimg.imread('Image.jpeg'))
+img=np.float32(mpimg.imread('visage.bmp'))
 img_data = img.reshape(-1, 3)  # Forme (256*256, 3)
 
 # Définir le nombre de couleurs souhaité
-k = 1000  # Nombre de clusters (attention ne pas en mettre trop sinon temps très long)
+k = 250  # Nombre de clusters (attention ne pas en mettre trop sinon temps très long)
 kmeans = KMeans(n_clusters=k, random_state=0).fit(img_data)
 
 # Obtenir les centres des clusters (nouvelles couleurs)
@@ -26,7 +26,7 @@ plt.imshow(np.uint8(img))  # Image d'origine
 
 plt.subplot(1, 2, 2)
 plt.title(f"Image compressée ({k} couleurs)")
-plt.imshow(compressed_img / 255)  # Marche sans le /255
+plt.imshow(compressed_img / 255)  # Image compressée
 
 
 plt.show()
